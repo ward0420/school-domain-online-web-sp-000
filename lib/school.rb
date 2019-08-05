3b788f4f0868fdd1edf43c69fa96efd533a3685d
Don Ward <10is0420@gmail.com>
2019-08-05 15:34:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+require 'pry'
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(students_name, grade) 
+    
+  end
+  binding.pry
+end
